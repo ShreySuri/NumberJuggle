512c7d5ce8e65246974b3928ce7607659b9996e9
Shrey Suri <shreysuri5@gmail.com>
2022-06-03 18:30:48 -0500

    sone with first mode:

--- NumberJuggle_1.py
+++ NumberJuggle_1.py
@@ -16,6 +16,7 @@ if user_input == "1":
     max_num = int(max_num)
 
     total = random.randint(1, max_num)
+    original_num = total
 
     end_game = ""
     turn_count = 0
@@ -23,15 +24,19 @@ if user_input == "1":
 
         operation = None
         while operation != "add" and operation != "subtract" and operation != "multiply":
+            print("")
             operation = input(print("Would you like to add, subtract, or multiply? "))
             operation = operation.lower()
 
         number = 0.5 
         while number % 1 != 0:
+            print("")
             number = input(print("What number would you like the computer to %s? Please enter an integer. " % operation))
             number = float(number)
         number = int(number)
 
+        print("")
+        
         if operation == "add":
             total = total + number
             print("%sing %s..." % (operation , number))
@@ -40,24 +45,41 @@ if user_input == "1":
             print("%sing %s..." % (operation , number))
         elif operation == "multiply":
             total = total * number
-            print("%sing %s..." % (operation , number))
+            print("%sing by %s..." % (operation , number))
         else:
             print("Something went wrong. ")
 
         time.sleep(3)
 
+        print("")
         print("If you would like the number to be revealed, type 'reveal'.")
         end_game = input(print("If you would like to continue, type anything else. "))
         end_game = end_game.lower()
         turn_count = turn_count + 1
 
-        if turn_count < 3:
+        if turn_count < 3 and end_game == "reveal":
+            print("")
             print("Are you sure you want the total to be revealed? Maybe a few more turns?")
             final = input(print("Type 'yes' or 'no'"))
             final = final.lower()
-            if final = "yes":
+            if final == "yes":
                 end_game = "reveal"
             else:
                 end_game = ""
 
-    print("The final total is %s, over the course of 
+    print("")
+    print("The final total is %s, over the course of %s turns." % (total, turn_count))
+
+    guess = 0.5
+    while guess % 1 != 0:
+        print("")
+        guess = input(print("Enter what you think the original number was. Please enter an integer. "))
+        guess = float(guess)
+    guess = int(guess)
+
+    print("")
+    
+    if guess == original_num:
+        print("You got it! If you would like to be challenged more, increase the max number next time you play.")
+    else:
+        print("Sorry, the original number was %s. You can lower the difficulty by decreasing the max number the next time you play.") 
