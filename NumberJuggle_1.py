4edac7a027efb10784785deb2e7ce46f8eec667a
Shrey Suri <shreysuri5@gmail.com>
2022-06-03 18:09:55 -0500

    almost done with first mode\

--- NumberJuggle_1.py
+++ NumberJuggle_1.py
@@ -1,10 +1,5 @@
-
-
-
-
-
-
-
+import random
+import time
 
 user_input = None
 
@@ -14,25 +9,43 @@ while user_input != "1" and user_input != "2":
 
 if user_input == "1":
 
-    operation = None
-    while operation != "add" and operation != "subtract" and operation != "multiply":
-        operation = input(print("Would you like the add, subtract, or multiply? "))
-        operation = operation.lower()
+    max_num = 0.5
+    while max_num % 1 != 0:
+        max_num = input(print("Up to what number do you want the computer to pick? Please type an integer. "))
+        max_num = float(max_num)
+    max_num = int(max_num)
 
-    number = 0.5 
-    while number % 1 != 0:
-        number = input(print("What number would you like computer to %s? Please enter an integer. " % operation))
-        number = float(number)
-    number = int(number)
+    total = random.randint(1, max_num)
 
-    if operation == "add":
-        total = total + number
-        print("%sing..." % operation)
-    elif operation == "subtract":
-        total = total - number
-        print("%sing..." % operation)
-    elif operation == "multiply":
-        total = total * number
-        print("%sing..." % operation)
-    else:
-        print("Something went wrong. ")
+    end_game = ""
+    while end_game != "reveal":
+        operation = None
+        while operation != "add" and operation != "subtract" and operation != "multiply":
+            operation = input(print("Would you like the add, subtract, or multiply? "))
+            operation = operation.lower()
+
+        number = 0.5 
+        while number % 1 != 0:
+            number = input(print("What number would you like computer to %s? Please enter an integer. " % operation))
+            number = float(number)
+        number = int(number)
+
+        if operation == "add":
+            total = total + number
+            print("%sing %s..." % (operation , number))
+        elif operation == "subtract":
+            total = total - number
+            print("%sing..." % operation)
+        elif operation == "multiply":
+            total = total * number
+            print("%sing..." % operation)
+        else:
+            print("Something went wrong. ")
+
+        time.sleep(3)
+
+        print("If you would like the number to be revealed, type 'reveal'.")
+        end_game = input(print("If you would like to continue, type anything else. "))
+        end_game = end_game.lower()
+
+    
